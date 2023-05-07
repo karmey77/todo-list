958b7a70c5e8729b00d5d9d21d9df97fb69d8abb
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-05-07 15:47:00 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 const exphbs = require('express-handlebars');
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+const Todo = require('./models/todo') // 載入 Todo model
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -26,8 +27,11 @@ db.once('open', () => {
 // 設定路由
 // Todo 首頁
 app.get('/', (req, res) => {
-    res.render('index')
-})
+    Todo.find() // 取出 Todo model 裡的所有資料
+      .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+      .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+      .catch(error => console.error(error)) // 錯誤處理
+  })
 
 // 設定 port 3000
 app.listen(3000, () => {
