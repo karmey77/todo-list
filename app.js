dde9051eac15e59f8770eb94ae9a1dc3b91e774b
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-05-07 16:08:54 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 const exphbs = require('express-handlebars');
+const Todo = require('./models/todo') // 載入 Todo model
+const bodyParser = require('body-parser')
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-const Todo = require('./models/todo') // 載入 Todo model
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -28,10 +33,21 @@ db.once('open', () => {
 // Todo 首頁
 app.get('/', (req, res) => {
     Todo.find() // 取出 Todo model 裡的所有資料
-      .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-      .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-      .catch(error => console.error(error)) // 錯誤處理
-  })
+        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+        .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+        .catch(error => console.error(error)) // 錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+    return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+    const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+    return Todo.create({ name })     // 存入資料庫
+        .then(() => res.redirect('/')) // 新增完成後導回首頁
+        .catch(error => console.log(error))
+})
 
 // 設定 port 3000
 app.listen(3000, () => {
